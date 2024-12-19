03e79fcb11595ecb6d1011f71735acd6b80c01a5
Ullrich <tobias.ullrich@FKIE>
2024-12-19 12:44:17 +0100

    button transition

--- pomodoro_timer/src/components/Timer.tsx
+++ pomodoro_timer/src/components/Timer.tsx
@@ -1,30 +1,30 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Countdown } from "./Countdown";
 import { TimerNav } from "./TimerNav";
-
 
 export const Timer = () => {
   // let pomodoroTime = 0 * 60;
   // let shortBreak = 5 * 60;
-// build later that i can also adjust timeframe of pomdoro
+  // build later that i can also adjust timeframe of pomdoro
 
-const POMODORO_TIME = 0.05*60
-const SHORT_BREAK = 0.05 * 60
-const LONG_BREAK = 0.05 * 60
+  const POMODORO_TIME = 0.05 * 60;
+  const SHORT_BREAK = 0.05 * 60;
+  const LONG_BREAK = 0.05 * 60;
   const [intervalId, setIntervalId] = useState<number | undefined>(undefined);
   const [pomodoroTime, setPomodoroTime] = useState<number>(POMODORO_TIME);
-  const[shortBreak, setShortBreak] = useState<number>(SHORT_BREAK)
-  const[longBreak, setLongBreak] = useState<number>(LONG_BREAK)
+  const [shortBreak, setShortBreak] = useState<number>(SHORT_BREAK);
+  const [longBreak, setLongBreak] = useState<number>(LONG_BREAK);
   const [active, setActive] = useState<boolean>(false);
-  const [round, setRound] = useState<number>(1)
-  const [session, setSession] = useState<"pomodoro" | "shortBreak" | "longBreak">("pomodoro")
- 
- 
-/**
- * Pomodoro 25 mins concentrate, 5 mins break (4x) -> 15 mins break
- */
+  const [round, setRound] = useState<number>(1);
+  const [session, setSession] = useState<
+    "pomodoro" | "shortBreak" | "longBreak"
+  >("pomodoro");
+
+  /**
+   * Pomodoro 25 mins concentrate, 5 mins break (4x) -> 15 mins break
+   */
   // countdown implementation
-  // beim countdown ist es momentan so, dass, wenn ich sehr oft auf pause drücke, es länger als der eigentliche countdown sein kann, weil die sekunde dann immer von vorne anfängt, das könnte ich noch mit date implentieren, dass pause drücken keinen einfluss auf meine Zeit hat 
+  // beim countdown ist es momentan so, dass, wenn ich sehr oft auf pause drücke, es länger als der eigentliche countdown sein kann, weil die sekunde dann immer von vorne anfängt, das könnte ich noch mit date implentieren, dass pause drücken keinen einfluss auf meine Zeit hat
   const handleButtonClick = () => {
     if (active) {
       clearInterval(intervalId);
@@ -48,40 +48,42 @@ const LONG_BREAK = 0.05 * 60
   useEffect(() => {
     if (pomodoroTime === 0 && session === "pomodoro") {
       clearInterval(intervalId);
-      round % 4 ? setSession("shortBreak") : setSession("longBreak")
+      round % 4 ? setSession("shortBreak") : setSession("longBreak");
       setPomodoroTime(POMODORO_TIME);
       setActive(false);
-      
     } else if (shortBreak === 0 && session === "shortBreak") {
       clearInterval(intervalId);
-      setSession("pomodoro")
+      setSession("pomodoro");
       setShortBreak(SHORT_BREAK);
       setActive(false);
-      setRound(round + 1)
-      
+      setRound(round + 1);
     } else if (longBreak === 0 && session === "longBreak") {
       clearInterval(intervalId);
       setSession("pomodoro");
       setLongBreak(LONG_BREAK);
       setActive(false);
-      setRound(round + 1)
+      setRound(round + 1);
     }
   }, [pomodoroTime, shortBreak, longBreak, session, round, intervalId]);
 
-
-  
   return (
     <div className={`timerContainer ${session}`}>
       <div className="timerWrapper">
-      <TimerNav currentSession={session} />
-      <Countdown currentTime={session === "pomodoro" ? pomodoroTime : session === "shortBreak" ? shortBreak : longBreak} />
-      <button className="startButton" onClick={handleButtonClick}>
-        {active ? "Pause" : "Start"}
-      </button>
-      
-    </div>
-    <a className="roundNumber"># {round}</a>
-  
+        <TimerNav currentSession={session} />
+        <Countdown
+          currentTime={
+            session === "pomodoro"
+              ? pomodoroTime
+              : session === "shortBreak"
+              ? shortBreak
+              : longBreak
+          }
+        />
+        <button className="startButton" onClick={handleButtonClick}>
+          {active ? "Pause" : "Start"}
+        </button>
+      </div>
+      <a className="roundNumber"># {round}</a>
     </div>
   );
 };
